bc1ab7d50adc6fa7b00a4aad9566615a6b01be5b
Maketsu17 <85955763+Maketsu17@users.noreply.github.com>
2021-10-12 10:20:18 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+jQuery('document').ready(function($){
+
+    var menuBtn = $('.menu-icon'), 
+    menu = $('.navigation Ul');
+    menuBtn.click(function(){
+    menu.addclass('show');
+
+    });
+});
